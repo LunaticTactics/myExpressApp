44e316d4b6d89790c6981bd7ef35c6c2165e5e1e
Franc <fngitzhofersds@gmail.com>
2022-10-11 10:57:20 -0400

    Ajout header

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lab 04', header: 'Ma super application Express' });
 });
 
 module.exports = router;
