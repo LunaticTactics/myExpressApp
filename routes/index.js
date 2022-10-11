7ec82d5021885347cafeafe0b21999d8c1e77eae
Franc <fngitzhofersds@gmail.com>
2022-10-11 11:15:46 -0400

    Fin lab4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lab 04', header: 'Ma super application Express' });
+  res.render('index', { title: 'Lab 04', header: 'Ma super application Express', text: 'Les membres de l\'équipe sont: Nicolas Payre et mon chien Patou' });
 });
 
 module.exports = router;
